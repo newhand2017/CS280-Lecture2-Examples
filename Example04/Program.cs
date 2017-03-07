f744b82bde934b9adb9c62c175105373a6f78536
newhand2017 <chivalry19850@gmail.com>
2017-03-07 21:16:13 +0800

    Example04 : 數字轉換未做錯誤處理

---
+++ Example04/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example04
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //---------------------------------------------
+            // 輸入錯誤格式
+            //---------------------------------------------
+
+            //---------------------------------------------
+            Console.Write("請輸入第一個數字 : ");
+            int number_1 = int.Parse(Console.ReadLine());
+
+            Console.Write("請輸入第二個數字 : ");
+            int number_2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0} + {1} = {2}", number_1, number_2, number_1 + number_2);
+
+            //---------------------------------------------
+        }
+    }
+}
