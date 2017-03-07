33b43ba12608a427caa977a179211301d5337f57
newhand2017 <chivalry19850@gmail.com>
2017-03-07 20:46:02 +0800

    Example03 : string 的用法

---
+++ Example03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example03
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string sql =
+                @"select * from table
+                where id > 1000";
+
+            string xml =
+                @"<Pokemon>
+                    <NationalN0>#001</NationalNo>
+                    <Name>Bulbsaur</Name>
+                  </Pokemon>";
+
+            string json =
+                @"Pokemon = {
+                    NationalN0: '#001'
+                    Name: Bulbsaur
+                 }";
+
+            Console.WriteLine(sql);
+            Console.WriteLine(xml);
+            Console.WriteLine(json);
+        }
+    }
+}
