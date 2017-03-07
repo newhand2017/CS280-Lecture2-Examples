8f75f2344f8f717050f5d0945e04fd60eeec7ecb
newhand2017 <chivalry19850@gmail.com>
2017-03-07 21:24:53 +0800

    增加輸入錯誤處理

--- Example04/Program.cs
+++ Example04/Program.cs
@@ -11,15 +11,31 @@ namespace Example04
         private static void Main(string[] args)
         {
             //---------------------------------------------
-            // 輸入錯誤格式
+            // Claim
             //---------------------------------------------
+            int number_1 = 0;
+            int number_2 = 0;
 
             //---------------------------------------------
             Console.Write("請輸入第一個數字 : ");
-            int number_1 = int.Parse(Console.ReadLine());
+            try
+            {
+                number_1 = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
 
             Console.Write("請輸入第二個數字 : ");
-            int number_2 = int.Parse(Console.ReadLine());
+            try
+            {
+                number_2 = int.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
 
             Console.WriteLine("{0} + {1} = {2}", number_1, number_2, number_1 + number_2);
 
